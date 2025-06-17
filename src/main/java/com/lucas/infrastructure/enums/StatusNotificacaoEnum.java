56fdc142f4847f146db19b332ff3e02c9ab80d75
Lucas Rodrigues <lucasrodrigues@MacBook-Air-de-Lucas.local>
2025-06-17 18:47:23 -0300

    adicionado camada de infra

---
+++ src/main/java/com/lucas/infrastructure/enums/StatusNotificacaoEnum.java
@@ -0,0 +1,8 @@
+package com.lucas.infrastructure.enums;
+
+public enum StatusNotificacaoEnum {
+
+    AGENDADO,
+    ENVIADO,
+    CANCELADO
+}
---
+++ src/main/java/com/lucas/infrastructure/repositories/AgendamentoRepository.java
@@ -0,0 +1,7 @@
+package com.lucas.infrastructure.repositories;
+
+import com.lucas.infrastructure.entities.Agendamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> {
+}
