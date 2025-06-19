760c1ba2905624b91e90bb4bb1c52fd257ae6d2c
Lucas Rodrigues <lucasrodrigues@MacBook-Air-de-Lucas.local>
2025-06-19 09:01:07 -0300

    ajustado classe principal e config do banco

--- src/main/java/com/lucas/controller/dto/out/AgendamentoRecordOut.java
+++ src/main/java/com/lucas/controller/dto/out/AgendamentoRecordOut.java
@@ -5,11 +5,12 @@ import com.lucas.infrastructure.enums.StatusNotificacaoEnum;
 
 import java.time.LocalDateTime;
 
-public record AgendamentoRecordOut(Long id,
-                                   String emailDestinatario,
-                                   String telefoneDestinatario,
-                                   String mensagem,
-                                   @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
-                                   LocalDateTime dataHoraEnvio,
-                                   StatusNotificacaoEnum statusNotificacao) {
+public record AgendamentoRecordOut
+        (Long id,
+           String emailDestinatario,
+           String telefoneDestinatario,
+           String mensagem,
+           @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+           LocalDateTime dataHoraEnvio,
+           StatusNotificacaoEnum statusNotificacao) {
 }
